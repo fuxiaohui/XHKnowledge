73c0755a78cc5ddbb2938305bf34f3e2ac9cfec1
Fuxiaohui <370471476@qq.com>
2015-08-13 08:38:30 +0800

    课程以外的知识点补充
    会定期更新一些大家在工作中可能会用的知识点

---
+++ TestFont/TestFont/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestFont
+//
+//  Created by fuxiaohui on 15/8/13.
+//  Copyright (c) 2015年 Henan Lanou Technology Co. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
